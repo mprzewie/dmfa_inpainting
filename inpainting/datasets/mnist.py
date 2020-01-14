5ff231cfefee4769d48a828a711d9d98ec13e882
Marcin Przewięźlikowski <m.przewie@gmail.com>
2020-01-14 21:13:53 +0100

    optional mask size randomization in MNIST

--- inpainting/datasets/mnist.py
+++ inpainting/datasets/mnist.py
@@ -6,9 +6,13 @@ import torch
 from torchvision.datasets import MNIST
 from torchvision import transforms as tr
 
-def random_mask_fn(mask_shape: Tuple[int, int]):
+def random_mask_fn(mask_shape: Tuple[int, int], mask_randomness: Tuple[int, int] = (0,0)):
     h, w = mask_shape
-    def tensor_to_tensor_with_random_mask(image_tensor):
+    hr, wr = mask_randomness
+    h += np.random.randint(-hr, hr)
+    w += np.random.randint(-wr, wr)
+
+    def tensor_to_tensor_with_random_mask(image_tensor: torch.Tensor):
         sx, sy = image_tensor.shape[1:]
         mask = np.ones((1, sx, sy))
         x, y = np.random.randint([0,0], [sx - h, sy - w])
@@ -17,10 +21,10 @@ def random_mask_fn(mask_shape: Tuple[int, int]):
     return tensor_to_tensor_with_random_mask
 
 
-def train_val_datasets(save_path: Path, mask_shape: Tuple[int, int] = (10, 10)) -> Tuple[MNIST, MNIST]:
+def train_val_datasets(save_path: Path, mask_shape: Tuple[int, int] = (10, 10), mask_randomness: Tuple[int, int] = (0,0)) -> Tuple[MNIST, MNIST]:
     transform = tr.Compose([
         tr.ToTensor(),
-        tr.Lambda(random_mask_fn(mask_shape=mask_shape))
+        tr.Lambda(random_mask_fn(mask_shape=mask_shape, mask_randomness=mask_randomness))
     ])
 
     ds_train = MNIST(save_path, train=True, download=True, transform=transform)
