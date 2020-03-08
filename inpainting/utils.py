9ad52afbdadbe6d07ec56388386509982a8d5d80
Marcin Przewięźlikowski <m.przewie@gmail.com>
2020-03-08 20:06:58 +0100

    predict on loader

--- inpainting/utils.py
+++ inpainting/utils.py
@@ -1,5 +1,9 @@
+from typing import Dict
+
 import numpy as np
 import torch
+from torch.utils.data.dataloader import DataLoader
+from tqdm import tqdm
 
 from inpainting.datasets.mask_coding import UNKNOWN_LOSS
 from inpainting.inpainters.inpainter import InpainterModule
@@ -39,3 +43,45 @@ def classifier_experiment(
         ),
         X_inpainted
     )
+
+
+def predictions_for_entire_loader(
+        inpainter: InpainterModule,
+        data_loader: DataLoader,
+        device: torch.device = torch.device("cpu")
+) -> Dict[str, np.ndarray]:
+    X = []
+    J = []
+    Y = []
+    P = []
+    M = []
+    A = []
+    D = []
+    for i, ((x, j), y) in tqdm(enumerate(data_loader)):
+        x, j, y = [t.to(device) for t in [x, j, y]]
+        p, m, a, d = inpainter(x, j)
+
+        for (x_, j_, y_, p_, m_, a_, d_) in zip(
+                x, j, y, p, m, a, d
+        ):
+            (x_, j_, y_, p_, m_, a_, d_) = [
+                t.detach().cpu().numpy()
+                for t in (x_, j_, y_, p_, m_, a_, d_)
+            ]
+            X.append(x_)
+            J.append(j_)
+            Y.append(y_)
+            P.append(p_)
+            M.append(m_)
+            A.append(a_)
+            D.append(d_)
+
+    return {
+        "X": np.array(X),
+        "J": np.array(J),
+        "Y": np.array(Y),
+        "P": np.array(P),
+        "M": np.array(M),
+        "A": np.array(A),
+        "D": np.array(D)
+    }
